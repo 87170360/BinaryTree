554964fbc6488bffea33c7b2ca00516157963d16
liushuxin <linux.book.heart@gmail.com>
2013-09-27 17:48:39 +0800

    test
    Signed-off-by: liushuxin <linux.book.heart@gmail.com>

--- BinaryTree.h
+++ BinaryTree.h
@@ -2,6 +2,7 @@
 #define __BINARYTREE_LSX_
 
 #include <vector>
+#include <map>
 #include <stddef.h>
 
 typedef std::vector<int> INTVEC;
@@ -30,15 +31,18 @@ pNode CreateBinaryTree(const INTVEC& vec, int i, int j)
 	if(i >= j) return(NULL);
 	pNode p = new Node;
 	p->value = vec[i];
-	std::cout << p->value << std::endl;
+	//std::cout << p->value << std::endl;
 	p->left = CreateBinaryTree(vec, 2 * i + 1, j);
 	p->right = CreateBinaryTree(vec, 2 * i + 2, j);
 
 	return p;
 }
 
+static std::map<int, INTVEC> m_tmp;
+
 void printBinaryTree(pNode p, int layer)
 {
+	
 	if(p == NULL) return;
 
 	printBinaryTree(p->right, ++layer);
@@ -47,7 +51,14 @@ void printBinaryTree(pNode p, int layer)
 	{
 		//std::cout << " ";
 	}
+	m_tmp[layer].push_back(p->value);
+	/*
 	std::cout << "layer:" << layer << " value:" << p->value << std::endl;
+	if(p->left)
+		std::cout << "  layer:" << layer + 1 << " left value:" << p->left->value << std::endl;
+	if(p->right)
+		std::cout << "  layer:" << layer + 1 << " right value:" << p->right->value << std::endl;
+	*/
 	printBinaryTree(p->left, ++layer);
 }
 
--- main.cpp
+++ main.cpp
@@ -8,17 +8,27 @@
 int main()
 {
 	INTVEC conf = parseInputFile();
+	/*
 	for(INTVEC_CITER iter = conf.begin(); iter != conf.end(); ++iter)
 	{
-		std::cout << *iter << " ";
+	std::cout << *iter << " ";
 	}
 	std::cout << std::endl;
+	*/
 
 	std::cout << "Create BinaryTree!" << std::endl;
 	pNode pRoot = CreateBinaryTree(conf, 0, conf.size());
 
 	std::cout << "Print BinaryTree!" << std::endl;
 	printBinaryTree(pRoot, 0);
+	for(std::map<int, INTVEC>::const_iterator mciter = m_tmp.begin(); mciter != m_tmp.end(); ++mciter)
+	{
+		for(INTVEC_CITER iter = mciter->second.begin(); iter != mciter->second.end(); ++iter)
+		{
+			std::cout << *iter << " ";
+		}
+		std::cout << std::endl;
+	}
 	return 0; 
 
 }
